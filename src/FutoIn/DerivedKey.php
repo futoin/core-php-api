5e15e83f1a544ba9e9428686ee11e51780431bc2
Andrey Galkin <andvgal@gmail.com>
2014-05-29 20:03:37 +0300

    Updated with spec changes

--- src/FutoIn/DerivedKey.php
+++ src/FutoIn/DerivedKey.php
@@ -7,7 +7,6 @@ interface DerivedKey
     public function getRaw();
     public function getBaseID();
     public function getSequenceID();
-    public function getSessionID();
     public function &encrypt( &$data );
     public function &decrypt( &$data );
 }
--- src/FutoIn/Executor/RequestInfo.php
+++ src/FutoIn/Executor/RequestInfo.php
@@ -3,11 +3,18 @@
 namespace FutoIn\Executor;
 
 interface RequestInfo {
+    const SL_ANONYMOUS = "Anonymous";
+    const SL_INFO = "Info";
+    const SL_SAFEOPS = "SafeOps";
+    const SL_PRIVLEGED_OPS = "PrivilegedOps";
+    const SL_EXCEPTIONAL_OPS = "ExceptionalOps";
+    
     public function &request();
     public function &response();
     public function &info();
     public function error( $name );
     public function getSecurityLevel();
+    public function isSecurityLevel( $lvl );
     public function getUser();
     public function getSourceAddress();
     public function getDerivedKey();
