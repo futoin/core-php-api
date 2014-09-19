81e79e7bde9a6577809889da989d7f4ca555fdb7
Andrey Galkin <andrey@futoin.eu>
2014-09-19 18:34:30 +0300

    Synchronized with FTN3 Iface Definition changes

--- src/FutoIn/DerivedKey.php
+++ src/FutoIn/DerivedKey.php
@@ -7,9 +7,11 @@ namespace FutoIn;
  */
 interface DerivedKey
 {
-    public function getRaw();
     public function getBaseID();
     public function getSequenceID();
-    public function &encrypt( &$data );
-    public function &decrypt( &$data );
+    public function encrypt( $data );
+    public function decrypt( $data );
+    public function encryptAsync( AsyncSteps as, $data );
+    public function decryptAsync( AsyncSteps as, $data );
+    
 }
--- src/FutoIn/Error.php
+++ src/FutoIn/Error.php
@@ -19,15 +19,83 @@ namespace FutoIn;
  */
 class Error extends \Exception
 {
+    /**
+     * Connection error before request is sent.
+     * Must be generated on Invoker side
+     */
     const ConnectError = "ConnectError";
+    
+    /**
+     * Communication error at any stage after request is sent
+     * and before response is received.
+     * Must be generated on Invoker side
+     */
     const CommError = "CommError";
-    const NotImplemented = "NotImplemented";
-    const Unauthorized = "Unauthorized";
-    const InternalError = "InternalError";
-    const InvokerError = "InvokerError";
+    
+    /**
+     * Unknown interface requested.
+     * Must be generated only on Executor side
+     */
     const UnknownInterface = "UnknownInterface";
+
+    /**
+     * Not supported interface version.
+     * Must be generated only on Executor side
+     */
     const NotSupportedVersion = "NotSupportedVersion";
+
+    /**
+     * In case interface function is not implemented on Executor side
+     * Must be generated on Executor side
+     */
+    const NotImplemented = "NotImplemented";
+
+    /**
+     * Security policy on Executor side does not allow to
+     * access interface or specific function.
+     * Must be generated only on Executor side
+     */
+    const Unauthorized = "Unauthorized";
+
+    /**
+     * Unexpected internal error on Executor side, including internal CommError.
+     * Must be generated only on Executor side
+     */
+    const InternalError = "InternalError";
+
+    /**
+     * Unexpected internal error on Invoker side, not related to CommError.
+     * Must be generated only on Invoker side
+     */
+    const InvokerError = "InvokerError";
+
+    /**
+     * Invalid data is passed as FutoIn request.
+     * Must be generated only on Executor side
+     */
     const InvalidRequest = "InvalidRequest";
+
+    /**
+     * Defense system has triggered rejection
+     * Must be generated on Executor side, but also possible to be triggered on Invoker
+     */
+    const DefenseRejected = "DefenseRejected";
+
+    /**
+     * Executor requests re-authorization
+     * Must be generated only on Executor side
+     */
+    const PleaseReauth = "PleaseReauth";
+
+    /**
+     * 'sec' request section has invalid data or not SecureChannel
+     * Must be generated only on Executor side
+     */
     const SecurityError = "SecurityError";
+
+    /**
+     * Timeout occurred in any stage
+     * Must be used only internally and should never travel in request message
+     */
     const Timeout = "Timeout";
 }
